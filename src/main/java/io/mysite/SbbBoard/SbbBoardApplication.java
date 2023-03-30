9d3ced555c4dae19f576b57f36acdad6a82e07c8
yeonann <lkim82@naver.com>
2023-03-30 19:41:07 +0900

    DB 연결

---
+++ src/main/java/io/mysite/SbbBoard/SbbBoardApplication.java
@@ -0,0 +1,13 @@
+package io.mysite.SbbBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mysite/SbbBoard/SbbBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mysite.SbbBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
